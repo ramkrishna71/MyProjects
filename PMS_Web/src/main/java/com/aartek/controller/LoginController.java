f8db484dc0c05c2e198394cfc3065b63bfabac16
ramkrishna71 <ramkrishna.gupta71@gmail.com>
2017-08-12 12:06:52 +0530

    loging method

--- PMS_Web/src/main/java/com/aartek/controller/LoginController.java
+++ PMS_Web/src/main/java/com/aartek/controller/LoginController.java
@@ -1,9 +1,18 @@
 package com.aartek.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
 
 public class LoginController {
+	
+	public String loginUser(Map map){
+		
+		return "ramakrihsna";
+		
+	}
 
 }
+
